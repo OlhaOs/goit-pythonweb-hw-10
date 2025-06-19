11c34400041043bc77042b219193c12431033a45
OlhaOA <olalaadka@gmail.com>
2025-06-19 20:20:01 +0300

    init

---
+++ src/databases/models.py
@@ -0,0 +1,44 @@
+import enum
+from .connect import Base, engine
+from sqlalchemy import Column, Integer, String, Date, Boolean, Enum, ForeignKey
+from sqlalchemy.orm import relationship
+
+
+class Role(enum.Enum):
+    admin: str = "admin"
+    moderator: str = "moderator"
+    user: str = "user"
+
+
+class Contact(Base):
+    __tablename__ = "contacts"
+    id = Column(Integer, primary_key=True, index=True)
+    first_name = Column(String(50), nullable=False)
+    last_name = Column(String(50), nullable=False)
+    email = Column(String(100), unique=True, nullable=False)
+    phone_number = Column(String(20), unique=True, nullable=False)
+    birthday = Column(Date, nullable=False)
+
+    user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+    user = relationship("User", back_populates="contacts")
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True, nullable=False)
+    password = Column(String, nullable=False)
+    roles = Column(Enum(Role), default=Role.user)
+    first_name = Column(String, nullable=True)
+    last_name = Column(String, nullable=True)
+    confirmed = Column(Boolean, default=False)
+    avatar = Column(String, nullable=True)
+
+    contacts = relationship(
+        "Contact", back_populates="user", cascade="all, delete-orphan"
+    )
+
+
+def init_db():
+    Base.metadata.create_all(bind=engine)
---
+++ src/settings/base.py
@@ -0,0 +1,15 @@
+import configparser
+import pathlib
+
+
+path = pathlib.Path(__file__).parent.joinpath("config.ini")
+
+parser = configparser.ConfigParser()
+parser.read(path)
+
+
+DB_URL = parser.get("DB", "DB_URL")
+
+
+SECRET_KEY = parser.get("AUTH", "SECRET_KEY")
+ALGORITHM = parser.get("AUTH", "ALGORITHM")
